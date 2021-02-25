a85f637edf339bfeaea76dd781b15fe8af7b41d1
Suraj Rai <surajrajrai@gmail.com>
2021-02-25 18:18:18 +0000

    updates to pages

--- src/templates/journal-template.js
+++ src/templates/journal-template.js
@@ -24,13 +24,10 @@ const JournalTemplate = props => {
         These are some articles I've written.
       </p>
       <p>
-        Topics I often explore are <a href="/tags/culture">culture</a>, <a href="/tags/science">science</a>, <a href="/tags/art">art</a>, <a href="/tags/data">data</a>, <a href="/tags/technology">technology</a> and <a href="/tags/productivity">productivity</a>. There maybe additional tags on articles which you are welcome to explore.
+        Topics I often explore are <a href="/tags/culture">culture</a>, <a href="/tags/science">science</a>, <a href="/tags/art">art</a>, <a href="/tags/data">data</a>, <a href="/tags/technology">technology</a> and <a href="/tags/productivity">productivity</a>. Some are <a href="/tags/longform">long</a>, some are <a href="/tags/short">short</a>, you may also be interested in some of my <a href="https://blog.logikblok.com/">historic writing</a>.
       </p>
       <p>
-        Some are <a href="/tags/longform">long</a>, some are <a href="/tags/short">short</a>, you may also be interested in some of my <a href="https://blog.logikblok.com/">historic writing</a>.
-      </p>
-      <p>
-        The images on posts are designed by me, you're welcome to explore them <a href="https://github.com/logikblok/bonneville/tree/master/src/content/images">here</a>.
+        The images are designed by me, you're welcome to explore them <a href="https://github.com/logikblok/bonneville/tree/master/src/content/images">here</a>.
         </p>
       <p>
         Feedback is always <a href="/contact">appreciated</a>.
