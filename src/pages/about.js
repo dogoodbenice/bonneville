e6d85b1ab31528de0cbcd40134983e364f80d107
Suraj Rai <surajrajrai@gmail.com>
2021-03-14 21:07:40 +0000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,12 +7,12 @@ const AboutPage = () => (
       Hello I'm Suraj. Professionally I'm a product manager and my current focus is on <a href="https://www.great.gov.uk/">making it easier for businesses to trade internationally</a>.
     </p>
     <p>
-      As a person I'm often most happiest working in teams looking to solve large problems for people using design, technology and data. 
+      As a person I'm happiest working in teams looking to solve large problems. Most often these are for people, however sometimes they can be for organisations or even machines. Overall I aim to create the best outcomes combining design, technology and data.
     </p>
     <p>
       You may like to explore my <a href="/projects">projects</a> or some of my <a href="/journal">thinking and writing</a>.
     </p>
-    <h4>Want to collaborate on something?</h4>
+    <h4>Shall we collaborate on something?</h4>
     <p>
       I'm always open for talking through ideas, problems and more. Feel free to send me a message using <a href="/contact">my contact details</a>.
     </p>
