b2ee435bc81804996b5d513dd4abbdbcc7c78802
Suraj Rai <surajrajrai@gmail.com>
2021-05-14 19:26:28 +0100

    Create elsewhere.js

---
+++ src/pages/elsewhere.js
@@ -0,0 +1,15 @@
+import React from "react"
+import SEO from "../components/SEO"
+
+const ElsewherePage = () => (
+  <>
+    <h1>Elsewhere on the internet</h1>
+    <p>This is me elsewhere on the interent.</p>
+    <br />
+    <p><a href="https://www.proverbproject.com">Ello</a></p>
+    <p><a href="https://www.proverbproject.com">Product Hunt</a></p>
+    <p>A project that created tools for schools.{" "}</p>
+  </>
+)
+
+export default ElsewherePage
