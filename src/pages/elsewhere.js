2d4a621c5b28b6b435e2b5e50efe80480ff66c76
Suraj Rai <surajrajrai@gmail.com>
2021-05-16 16:30:30 +0100

    Update elsewhere.js

--- src/pages/elsewhere.js
+++ src/pages/elsewhere.js
@@ -4,16 +4,15 @@ import SEO from "../components/SEO"
 const ElsewherePage = () => (
   <>
     <h1>Elsewhere on the internet</h1>
-    <p>If you'd like to hang out digitally, this me elsewhere on the internet.</p>
+    <p>If you'd like to hang out digitally.</p>
     <br />
-    <p><a href="https://www.proverbproject.com">Ello</a></p>
-    <p><a href="https://www.proverbproject.com">Product Hunt</a></p>
-    <p><a href="https://www.proverbproject.com">Youtube</a></p>
-    <p><a href="https://www.proverbproject.com">Instagram</a></p>
+    <p><a href="https://ello.co/logikblok">Ello</a></p>
+    <p><a href="https://www.producthunt.com/@logikblok">Product Hunt</a></p>
+    <p><a href="https://www.youtube.com/c/logikblok">Youtube</a></p>
+    <p><a href="https://www.instagram.com/logikblok">Instagram</a></p>
     <p><a href="https://old.reddit.com/user/logikblok/">Reddit</a></p>
-    <p><a href="https://www.proverbproject.com">Tik Tok</a></p>
-    <p><a href="https://www.proverbproject.com">Tumblr</a></p>
-    <p>A project that created tools for schools.{" "}</p>
+    <p><a href="https://www.tiktok.com/@logikblok">Tik Tok</a></p>
+    <p><a href="https://logikblok-blog.tumblr.com/">Tumblr</a></p>
   </>
 )
 
