3e5ead809413537c4ede9538bf906e6e4658bd16
Suraj Rai <surajrajrai@gmail.com>
2021-05-16 14:14:06 +0100

    Update elsewhere.js

--- src/pages/elsewhere.js
+++ src/pages/elsewhere.js
@@ -4,10 +4,15 @@ import SEO from "../components/SEO"
 const ElsewherePage = () => (
   <>
     <h1>Elsewhere on the internet</h1>
-    <p>This is me elsewhere on the interent.</p>
+    <p>If you'd like to hang out digitally, this me elsewhere on the internet.</p>
     <br />
     <p><a href="https://www.proverbproject.com">Ello</a></p>
     <p><a href="https://www.proverbproject.com">Product Hunt</a></p>
+    <p><a href="https://www.proverbproject.com">Youtube</a></p>
+    <p><a href="https://www.proverbproject.com">Instagram</a></p>
+    <p><a href="https://old.reddit.com/user/logikblok/">Reddit</a></p>
+    <p><a href="https://www.proverbproject.com">Tik Tok</a></p>
+    <p><a href="https://www.proverbproject.com">Tumblr</a></p>
     <p>A project that created tools for schools.{" "}</p>
   </>
 )
