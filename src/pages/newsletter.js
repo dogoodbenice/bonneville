14fcb9af0a0ab574b0327db532b23bca64206f59
Suraj Rai <surajrajrai@gmail.com>
2025-04-03 10:08:07 +0100

    Update newsletter.js

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,7 @@
 import React from "react"
 import SEO from "../components/SEO"
 
-const newsletterPage = () => (
+const NewsletterPage = () => (
   <>
     <h1>My occasional newsletter</h1>
     <p>I send out a newsletter every few months with some of my thoughts and projects. If you'd like to receive it, please sign up below.</p>
@@ -11,4 +11,4 @@ const newsletterPage = () => (
   </>
 )
 
-export default ElsewherePage
+export default NewsletterPage
