edc8aef8b06055ae209ae759913bc4a0a45b8db0
Suraj Rai <surajrajrai@gmail.com>
2025-04-08 12:18:49 +0100

    Update newsletter.js

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import SEO from "../components/SEO"
 import styled from "styled-components"
 
@@ -8,20 +8,6 @@ const FormContainer = styled.div`
 `
 
 const NewsletterPage = () => {
-  useEffect(() => {
-    // Load the external script
-    const script = document.createElement('script')
-    script.src = "https://eocampaign1.com/form/612a5b24-1467-11f0-9fc2-35c57a1f5070.js"
-    script.async = true
-    script.dataset.form = "612a5b24-1467-11f0-9fc2-35c57a1f5070"
-    document.body.appendChild(script)
-
-    // Cleanup
-    return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
-
   return (
     <>
       <SEO title="Newsletter" />
@@ -30,7 +16,11 @@ const NewsletterPage = () => {
       <p>If you'd like to receive these letters, please sign up below. Or use <a href="https://surajr.eo.page/hello">this direct link to sign up</a>.</p>
       <br />
       <FormContainer>
-        <div id="form-612a5b24-1467-11f0-9fc2-35c57a1f5070"></div>
+        <script 
+          async 
+          src="https://eocampaign1.com/form/612a5b24-1467-11f0-9fc2-35c57a1f5070.js" 
+          data-form="612a5b24-1467-11f0-9fc2-35c57a1f5070"
+        ></script>
       </FormContainer>
       <br />
       <br />
