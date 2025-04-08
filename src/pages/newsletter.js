43c0c366dc05c97560da9035f9679c2461e32892
Suraj Rai <surajrajrai@gmail.com>
2025-04-08 11:11:21 +0100

    Newsletter updates

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -6,7 +6,9 @@ const NewsletterPage = () => (
     <h1>My occasional newsletter</h1>
     <p>I send out a newsletter every few months with some of my thoughts and projects. If you'd like to receive it, please sign up below.</p>
     <br />
-    Watch this space 
+    <iframe src="https://surajr.eo.page/hello" title="short letters from Suraj"></iframe>
+    <br />  
+    <a href="https://surajr.eo.page/hello">Direct link to sign up</a>.
     <br />  
   </>
 )
---
+++ src/pages/newsletterthanks.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SEO from "../components/SEO"
+
+const NewsletterThankyouPage = () => (
+  <>
+    <h1>Thanks for signing up!</h1>
+    <p>Here's a super secret thank you</p>
+    <br />
+    Watch this space 
+    <br />  
+  </>
+)
+
+export default NewsletterThankyouPage
