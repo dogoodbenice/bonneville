ffd941b40d1967080b65011201f0224864eeef51
Suraj Rai <surajrajrai@gmail.com>
2025-04-22 16:32:31 +0100

    Update about.js
    Content updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,9 @@ const AboutPage = () => (
     <h1>About Me</h1>
     <p>Hello I'm Suraj.</p>
     <p>
-      Professionally I'm a product manager and my current focus is on <a href="https://humanisingautonomy.com/">building easy interfaces for human and machine interaction using computer vision</a>.
+      Professionally I'm a product manager and my current focus is on <a href="https://cube.global/solutions/technology">creating understandable and actionable insights for people on planetscale data</a>.
     </p>
-    <p>I am a trustee and board member for the <a href="https://integratedtransport.org.uk/">Foundation for Integrated Transport</a>, through which I support grant making and impact investment into sustainable and transformational transport solutions.</p>
+    <p>I am a trustee and board member for the <a href="https://integratedtransport.org.uk/">Foundation for Integrated Transport</a>, through which I support transformational grant making and impact investment into sustainable transport solutions within the UK.</p>
     <p>As a person I'm happiest working in teams looking to solve large problems. These can be for people, organisations or even machines. Overall I aim to create the best outcomes combining design, technology and data.</p>
     <p>You may like to explore my <a href="/projects">projects</a> or some of my <a href="/journal">thinking and writing</a>, sign up to my occasional <a href="/newsletter">newsletter</a>, or sign my <a href="/guestbook">guestbook</a>?</p>
     <h4>Shall we collaborate on something?</h4>
